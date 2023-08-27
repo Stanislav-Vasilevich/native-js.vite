02d93f4a6994a1ed4e9dc59f4de3e23d46a0d745
Stanislav <stanislavasilevich@gmail.com>
2023-08-27 22:01:31 +0300

    написал собственные методы массивов

--- src/js/index.js
+++ src/js/index.js
@@ -1,99 +1,151 @@
-console.log('typeof \'hello\': ', typeof 'hello'); // '', "", ``
-console.log('typeof 43: ', typeof 43); // NaN, infinity, -infinity,
-console.log('typeof undefined: ', typeof undefined); // undefined
-console.log('typeof true: ', typeof true); // false, true
-console.log('typeof null: ', typeof null); // null
-console.log('typeof {name: \'Alex\'}: ', typeof {name: 'Alex'}); // object, array, function
-console.log('typeof 9007199254740991n: ', typeof 9007199254740991n); // bigInt
+const students = [
+  {
+    name: "Bob",
+    age: 22,
+    isMarried: true,
+    scores: 85,
 
-const num = 3;
-
-const user = {
-  name: 'bob',
-}
-
-const arr = [
-  user,
-  {name: 'Alex'}
+  },
+  {
+    name: "Alex",
+    age: 21,
+    isMarried: true,
+    // scores: 89
+  },
+  {
+    name: "Nick",
+    age: 20,
+    isMarried: false,
+    scores: 120
+  },
+  {
+    name: "John",
+    age: 19,
+    isMarried: false,
+    scores: 100
+  },
+  ''
 ];
 
-// C - create
-// R - read
-// U - update
-// D - delete
+function map(array, fn) {
+  const arr = [];
+  for(let i = 0; i < array.length; i++) {
+    arr[i] = fn(array[i]);
+  }
 
-// мутабельно и иммутабельно
-
-const users = [
-  {
-    id: 1,
-    name: "Bob",
-    isStudent: true,
-  },
-  {
-    id: 2,
-    name: "Alex",
-    isStudent: true,
-  },
-  {
-    id: 3,
-    name: "Ann",
-    isStudent: true,
-  },
-  {
-    id: 4,
-    name: "Donald",
-    isStudent: true,
-  },
-]
-
-const newUser = {
-  id: 5,
-  name: "Farid",
-  isStudent: true,
+  return arr;
 }
 
-// users.push(newUser); // мутабельно
-const newObject = [...users, newUser];
+// console.log(map(students, st => st.name));
 
-console.log('newObject: ', newObject); // иммутабельно
-
-
-const superUser = {
-  "id": 1,
-  "name": "Leanne Graham",
-  "username": "Bret",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
+function filter(array, fn) {
+  const newArr = [];
+  for(let i = 0; i < array.length; i++) {
+    if(fn(array[i])) {
+      newArr.push(array[i]);
     }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
   }
-};
 
-const newSuperUser = {...superUser, phone: "2000"}
+  return newArr;
+}
 
-console.log('newSuperUser: ', newSuperUser);
-console.log(newSuperUser === superUser);
+// console.log(filter(students, st => st.name.length > 4));
 
-const newSuperUser2 = {...superUser, address: {...superUser.address, suite: "hello"}};
+function find(array, fn) {
+  for(let i = 0; i < array.length; i++) {
+    if(fn(array[i])) {
+      return array[i];
+    }
+  }
+}
 
-console.log('newSuperUser2: ', newSuperUser2);
-console.log(newSuperUser === newSuperUser2);
+console.log(find(students, st => st.name === 'Nick'));
 
-const newSuperUser3 = {...superUser, address: {...superUser.address, geo: {...superUser.address.geo, lat: "hello))"}}}
+function Fun() {
+  this.name = 'Alex';
+  this.age = 23;
+}
 
-console.log('newSuperUser3: ', newSuperUser3);
-console.log(newSuperUser2 === newSuperUser3);
+console.log(new Fun());
+
+const itPush = (array, newItem) => {
+  array[array.length] = newItem;
+
+  return array.length;
+}
+
+console.log(itPush([1, 2, 3], 4));
+
+const arr = [2, 3, 4];
+
+const itPop = (array) => {
+  const lastItem = array.length -1;
+  array.length = array.length - 1;
+
+  return lastItem;
+}
+
+console.log(itPop(arr));
+console.log(arr);
+
+const arr2 = [3, 4, 5, 6];
+
+const itReverse = (array) => {
+  const result = [];
+  for(let i = 0; i < array.length; i++) {
+    result[array.length - 1 - i] = array[i];
+  }
+  return result;
+}
+
+console.log(itReverse(arr2));
+
+const arr3 = [2, 3, 4, 5, 6];
+
+const itToReverse = (array) => {
+  for(let i = 0; i < Math.floor(array.length / 2); i++) {
+    const temp = array[array.length - 1 - i];
+    array[array.length = 1 - i] = array[i];
+    array[i] = temp;
+  }
+}
+
+itToReverse(arr3)
+console.log(arr3);
+
+const arr4 = [2, 5, 45, 67, 6];
+
+const itSlice = (arr, startIndex = 0, endIndex = arr.length) => {
+  const result = [];
+
+  for(let i = startIndex; i < endIndex; i++) {
+    result[i - startIndex] = arr[i];
+  }
+
+  return result;
+}
+
+console.log(itSlice(arr4, 0, 2));
+console.log(itSlice(arr4));
+
+const itIncludes = (array, elem, startIndex = 0) => {
+  const start = startIndex < 0 ? array.length + startIndex : startIndex;
+
+  for (let i = start; i < array.length; i++) {
+    if(elem === array[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const arr5 = [2, 5, 45, 67, 6];
+
+console.log(itIncludes(arr5, 5));
+console.log(itIncludes(arr5, 0));
+console.log(itIncludes(arr5, 2, 1));
+console.log(itIncludes(arr5, 2, -5));
+
+const itIndexOf = () => {
+
+}
